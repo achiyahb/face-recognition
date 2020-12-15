38c6b1b6e01f9e417ccbc1551d8a89d23ebf6869
achiyahb <achiyahb@gmail.com>
2020-12-15 21:47:42 +0200

    start to change the signIn.js to be more react likely by setState and stuff like that

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,7 +1,7 @@
 import React from "react";
 import setRegister from "../../smartBrainApi/registerMethod";
 
-const Register = ({onRouteChange}) => {
+class Register extends React.component{
     const onPressSubmit = ()=>{
         let password = document.getElementById('password').value
         let email = document.getElementById('email-address').value
@@ -9,46 +9,50 @@ const Register = ({onRouteChange}) => {
         setRegister(password,email,name,onRouteChange)
     }
 
-    return (
-        <article className="br3 ba  b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-            <main className="pa4 black-80">
-                <div className="measure">
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f1 fw6 ph0 mh0">Register</legend>
-                        <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="user-name">Your Name</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                                   type="email"
-                                   name="email-address" id="name"/>
+    render(){
+        const {onRouteChange} = this.state
+        return (
+            <article className="br3 ba  b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+                <main className="pa4 black-80">
+                    <div className="measure">
+                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                            <legend className="f1 fw6 ph0 mh0">Register</legend>
+                            <div className="mt3">
+                                <label className="db fw6 lh-copy f6" htmlFor="user-name">Your Name</label>
+                                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                       type="email"
+                                       name="email-address" id="name"/>
+                            </div>
+                            <div className="mt3">
+                                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                       type="email"
+                                       name="email-address" id="email-address"/>
+                            </div>
+                            <div className="mv3">
+                                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                       type="password"
+                                       name="password" id="password"/>
+                            </div>
+                        </fieldset>
+                        <div className="">
+                            <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                                   onClick={() => onPressSubmit()}
+                                   type="submit"
+                                   value="Register"/>
                         </div>
-                        <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                                   type="email"
-                                   name="email-address" id="email-address"/>
+                        <div className="lh-copy mt3">
+                            <p
+                                onClick={() => onRouteChange('signIn')}
+                                className="f6 link dim black db pointer">signIn</p>
                         </div>
-                        <div className="mv3">
-                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                                   type="password"
-                                   name="password" id="password"/>
-                        </div>
-                    </fieldset>
-                    <div className="">
-                        <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                               onClick={() => onPressSubmit()}
-                               type="submit"
-                               value="Register"/>
                     </div>
-                    <div className="lh-copy mt3">
-                        <p
-                            onClick={() => onRouteChange('signIn')}
-                            className="f6 link dim black db pointer">signIn</p>
-                    </div>
-                </div>
-            </main>
-        </article>
-    )
+                </main>
+            </article>
+        )
+    }
+
 
 
 }
